e963f3a63671b76de7ccca575670da878eb142c0
Jordan Foose <jordan.foose@chasacademy.se>
2026-02-14 00:03:01 +0100

    fix eslint config and enable jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,25 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  // Main JS config
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  // Jest test files config
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
 ]);
